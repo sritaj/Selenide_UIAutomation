634ddaf42fe2b1969fa9ab34dc61526b35948f7b
Sritaj Kumar Patel <sritajp@gmail.com>
2023-04-25 02:16:46 +0530

    Updated page with more navigation methods

--- src/main/java/pages/EmployeeInformationPage.java
+++ src/main/java/pages/EmployeeInformationPage.java
@@ -1,6 +1,8 @@
 package pages;
 
 import com.codeborne.selenide.SelenideElement;
+import pages.subpagecomponents.employeesubpages.AddEmployeePage;
+import pages.subpagecomponents.employeesubpages.EmployeeListPage;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byTagAndText;
@@ -9,8 +11,15 @@ import static com.codeborne.selenide.Selenide.$;
 public class EmployeeInformationPage {
 
     private static final SelenideElement ADD_EMPLOYEE_LINK = $(byTagAndText("a", "Add Employee"));
+    private static final SelenideElement EMPLOYEE_LIST_LINK = $(byTagAndText("a", "Employee List"));
 
-    public void addNewEmployee() {
+    public AddEmployeePage navigateToAddEmployeePage() {
         ADD_EMPLOYEE_LINK.shouldBe(visible).click();
+        return new AddEmployeePage();
+    }
+
+    public EmployeeListPage navigateToEmployeeListPage() {
+        EMPLOYEE_LIST_LINK.shouldBe(visible).click();
+        return new EmployeeListPage();
     }
 }
