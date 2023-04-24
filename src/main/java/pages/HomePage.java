1483517077e9bd0b4e2d2ab7be2c564e2102c63c
Sritaj Kumar Patel <sritajp@gmail.com>
2023-04-25 00:33:25 +0530

    Added Home Page

---
+++ src/main/java/pages/HomePage.java
@@ -0,0 +1,13 @@
+package pages;
+
+import pages.pagecomponents.LeftMenuComponent;
+
+public class HomePage {
+
+    private LeftMenuComponent leftMenuComponent;
+
+
+    public LeftMenuComponent getLeftMenuComponent() {
+        return leftMenuComponent;
+    }
+}
