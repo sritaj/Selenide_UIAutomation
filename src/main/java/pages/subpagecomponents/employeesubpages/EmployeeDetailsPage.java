a1b12d3536572e60895512f313f1bdc7530cf5a4
Sritaj Kumar Patel <sritajp@gmail.com>
2023-04-25 02:18:13 +0530

    Added EmployeeDetailsPage and added relevant methods to validateEmployee

---
+++ src/main/java/pages/subpagecomponents/employeesubpages/EmployeeDetailsPage.java
@@ -0,0 +1,24 @@
+package pages.subpagecomponents.employeesubpages;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byTagName;
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selenide.*;
+
+public class EmployeeDetailsPage {
+
+    private static final SelenideElement HEADER = $$(byTagName("h6")).get(1);
+    private static final SelenideElement PERSONAL_DETAILS_TAB = $(byXpath("//a[normalize-space()='Personal Details']"));
+
+    public void checkIfPageIsLoaded() {
+        Wait().until(webDriver -> PERSONAL_DETAILS_TAB.shouldBe(visible));
+    }
+
+    public void verifyEmployeeDetails(String employeeFullName) {
+        checkIfPageIsLoaded();
+        HEADER.shouldHave(visible).shouldHave(text(employeeFullName));
+    }
+}
