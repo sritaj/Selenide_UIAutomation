2ed9f332b2fff859ed4bf817316dae183e78a7d2
Sritaj Kumar Patel <sritajp@gmail.com>
2023-04-28 13:14:50 +0530

    Added TextReport in BaseTest so it can be utilized by all Tests

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,17 +1,16 @@
 package base;
 
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.junit5.TextReportExtension;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.ExtendWith;
 import utils.CleanupImpl;
 
-import java.io.File;
-
-import static com.codeborne.selenide.Configuration.*;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.open;
+
+@ExtendWith({TextReportExtension.class})
 public class BaseTest {
 
     protected BaseTest() {
@@ -19,7 +18,7 @@ public class BaseTest {
     }
 
     @BeforeAll
-    public static void setup(){
+    public static void setup() {
         CleanupImpl.cleanUpDirectory("/build/reports/tests");
         open("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
         //browserSize = "1920*1080";
@@ -27,7 +26,7 @@ public class BaseTest {
     }
 
     @AfterAll
-    public static void teardown(){
+    public static void teardown() {
         closeWebDriver();
     }
 }
